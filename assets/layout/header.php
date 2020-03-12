45f6b748631220e54b18e711ca507bc220935d90
Guezzy <31390723+iguezzy@users.noreply.github.com>
2020-03-12 18:55:22 +0600

    Add files via upload

--- assets/layout/header.php
+++ assets/layout/header.php
@@ -5,33 +5,80 @@
 		$iPage = 'Регистрация';	
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>Guezzy | <? echo $iPage ?></title>
+    <!DOCTYPE html>
+    <html lang="en">
 
-	<!-- CSS -->
-	<link rel="stylesheet" href="/assets/css/style.css">
-	
-	<!-- JS -->
-	<script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
-</head>
-<body>
-	<header>
-		<div class="logo">
-			<img src="https://yt3.ggpht.com/a/AGF-l7-JbZj98wJ3pQkgJhgmkdt0cLeScTkdVDdGlQ=s900-c-k-c0xffffffff-no-rj-mo" alt="">
-		</div>
+    <head>
+        <meta charset="UTF-8">
+        <title>Guezzy |
+            <? echo $iPage ?>
+        </title>
 
-		<div class="menu">
-			<div class="navbar">
-				<ul class="nav-menu">
-					<li>Главная</li>
-					<li>О компании</li>
-					<li>Офферы</li>
-					<li>Контакты</li>
-					<li>5</li>
-				</ul>
-			</div>
-		</div>
-	</header>
+        <!-- CSS -->
+        <link rel="stylesheet" href="/assets/css/style.css">
+
+        <!-- JS -->
+        <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+        <!-- Font Awesome -->
+        <script src="https://kit.fontawesome.com/ada758ae4a.js" crossorigin="anonymous"></script>
+
+        <!-- BootStrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+        <!-- Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+        <!-- JQuery -->
+        <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
+    </head>
+
+    <body>
+        <header class="container">
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand" href="#">
+                    <div class="logo">
+                        <img src="https://yt3.ggpht.com/a/AGF-l7-JbZj98wJ3pQkgJhgmkdt0cLeScTkdVDdGlQ=s900-c-k-c0xffffffff-no-rj-mo" alt="">
+                    </div>
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>	
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Главная <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">О компании</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Офферы</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Контакты</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+                    </ul>
+                    <div class="row">
+                    	<ul class="navbar-nav mr-auto">
+                    	<li class="nav-item">
+                            <a class="nav-link" href="#">Войти</a>
+                        </li>
+                        <li class="nav-item ml-4">
+                            <a href=""><button class="register">Регистрация</button></a>
+                        </li>
+                    	</ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
